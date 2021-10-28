c5e54c035c2ad710b566a28c7b11572043bf1312
9BristolMyers <88287985+9BristolMyers@users.noreply.github.com>
2021-10-28 15:01:42 +0700

    Add files via upload
    Ну, почему мой commit не отображается в GitHub'е?!..

---
+++ Lesson1.3/vk-api/Modules/Friends/Models/Friend4.swift
@@ -0,0 +1,27 @@
+//
+//  Friend4.swift
+//  Lesson1.3Tests
+//
+//  Created by Владимир Рузавин on 10/28/21.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Friend4: Codable {
+    let id: Int
+    let lastName: String
+    let trackCode, firstName: String
+    let photo100: String
+    
+    var fullName: String {
+        firstName + lastName
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case lastName = "last_name"
+        case firstName = "first_name"
+        case photo100 = "photo_100"
+    }
+}
